1c89bbc32e4075e9abd26875387b0e10144f03f2
JaganMohan <mohan.kreddy301@gmail.com>
2025-11-07 19:17:21 +0530

    calling the API and storing the data locally

---
+++ constants/utils.js
@@ -0,0 +1,4 @@
+export const Utilis = {
+  BASE_URL: "https://api.pexels.com/v1/search?query=people&page=1&per_page=30",
+  API_KEY: "",
+};
---
+++ js/script.js
@@ -0,0 +1,55 @@
+import { Utilis } from "../constants/utils.js";
+
+const resetButton = document.querySelector("#clear-local-data");
+const profileImg = document.querySelector('#profile-image');
+
+resetButton.addEventListener("click", () => {
+  const storedData = localStorage.getItem("pexels_photo_data");
+  if (storedData) {
+    localStorage.removeItem("pexels_photo_data");
+  } else {
+    alert("Local copy is not available");
+  }
+});
+
+const fetchImagesFromAPI = async () => {
+  const BASE_URL = Utilis.BASE_URL;
+  const API_KEY = Utilis.API_KEY;
+
+  try {
+    const response = await fetch(BASE_URL, {
+      headers: {
+        Authorization: API_KEY,
+      },
+    });
+    const data = await response.json();
+    const jsonString = JSON.stringify(data);
+    localStorage.setItem("pexels_photo_data", jsonString);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const displayImages = (photos) => {
+  if (photos.length > 0) {
+    const src = photos[0].src;
+    const url = src.original;
+    if (url) {
+      console.log(url);
+      profileImg.src = url; 
+    }
+  }
+};
+
+const fetchImagesFromLocalStorage = () => {
+  const storedData = localStorage.getItem("pexels_photo_data");
+  localStorage.setItem("copy_pexels_photo_data", storedData);
+  let dataObject = null;
+  if (storedData) {
+    dataObject = JSON.parse(storedData);
+    const { prev_page, photos, next_page, per_page, page } = dataObject;
+    displayImages(photos);
+  }
+};
+
+fetchImagesFromLocalStorage();
